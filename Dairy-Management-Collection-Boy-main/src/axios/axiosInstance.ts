1a341d89f53c16f6eef8417856215962a1155a17
Demo <kartikkumbhar1812@gmail.com>
2026-02-09 16:29:57 +0530

    url

--- Dairy-Management-Collection-Boy-main/src/axios/axiosInstance.ts
+++ Dairy-Management-Collection-Boy-main/src/axios/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // import.meta.env.VITE_API_BASE_URL || 
-  baseURL:import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  // import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
+  baseURL:import.meta.env.VITE_API_BASE_URL ,
   withCredentials: true,
 });
 
